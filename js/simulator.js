47bbe5aacd5d2db4e220517ea8f1c7f953b729d9
rojasmcamilo <cristhianrojasm@gmail.com>
2021-09-07 06:28:38 -0500

    actualizacio de pagina

--- js/simulator.js
+++ js/simulator.js
@@ -1,16 +1,33 @@
 let calcular = document.querySelector("#submit");
 let igsueldo = document.querySelector("#igsueldo");
-let igservicios = document.querySelector("#igservicios ");
+let igservicios = document.querySelector("#igservicios");
 let igprofesionales = document.querySelector("#igprofesionales ");
 let igpensiones = document.querySelector("#igpensiones");
 let iginversiones = document.querySelector("#iginversiones");
 let igotros = document.querySelector("#igotros");
-let igresult = document.querySelector("#igresult");
+let toingresos = document.querySelector("#toingresos");
+let gaalquiler = document.querySelector("#gaalquiler");
+let gahogar = document.querySelector("#gahogar");
+let gaalimentacion = document.querySelector("#gaalimentacion");
+let gaeducacion = document.querySelector("#gaeducacion");
+let gaotros = document.querySelector("#gaotros");
+let togastos = document.querySelector("#togastos");
+let depagos = document.querySelector("#depagos");
+let deprestamos = document.querySelector("#deprestamos");
+let defacturas = document.querySelector("#defacturas");
+let dehipoteca = document.querySelector("#dehipoteca");
+let deotros = document.querySelector("#deotros");
+let todeudas = document.querySelector("#todeudas");
+let fineto = document.querySelector("#fineto");
+let fideudas = document.querySelector("#fideudas");
+let fiahorro = document.querySelector("#fiahorro");
+let simulador = document.querySelector("#simulador");
 
-
-
-calcular.addEventListener("click", function(e){
-    console.log("Hola estoy uncionado ");
-    e.preventDefault(),
-    igresult.innerHtml = igsueldo.value.parseInt()+igservicios.value.parseInt()+igprofesionales.value+igpensiones.value+iginversiones.value+igotros.value
+calcular.addEventListener('click', function(e){ 
+    e.preventDefault();
+    alert("error");
+    alert(igsueldo.value.parseInt());
+    console.log(igsueldo.value.number());
+    toingresos.innerHTML = igsueldo.value.number() +igservicios.value.number()+igprofesionales.value.number()+igpensiones.value.number() + iginversiones.value.number() + igotros.value.number()
 })
+
